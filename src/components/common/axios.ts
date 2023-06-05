edb49ed72d740ba72e1726a75e5389f2937f3b5b
deepakpaliwal <deepak.paliwal@redbus.com>
2023-06-05 21:48:04 +0530

    remove crendtials

--- src/components/common/axios.ts
+++ src/components/common/axios.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
-const BASE_URL = 'https://todo-backend-ltb3ddfvx-devil94101.vercel.app';
+const BASE_URL = 'http://localhost:3001/';
 // const BASE_URL = "https://todo-backend-ts.vercel.app/"
 export default axios.create({
     baseURL: BASE_URL,
-    withCredentials: true
+    // withCredentials: true
 });
 
 export const axiosPrivate = axios.create({
     baseURL: BASE_URL,
-    headers: { 'Content-Type': 'application/json' },
-    withCredentials: true
+    headers: { 'Content-Type': 'application/json', },
+    // withCredentials: true
 });
